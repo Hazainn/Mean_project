678178c35913e905a14a7068a302f229a59c724e
William LA PORTE <laport_w@etna-alternance.net>
2015-06-12 14:47:51 +0200

    suite
    [norme] suite des modifications

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err    = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -47,7 +47,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if ("development" === app.get('env')) {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
