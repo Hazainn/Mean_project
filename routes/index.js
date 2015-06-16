fbf47d0255140278e5f0dc67bb89878b8d4c4bd2
William LA PORTE <laport_w@etna-alternance.net>
2015-06-16 18:02:16 +0200

    API en marche
    API en marche

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router  = express.Router();
+var title   = 'Blog Jahwess';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Blog Jahwess' });
+  res.render('index', { title: title });
 });
 
 router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Blog Jahwess' });
+  res.render('test', { title: title });
 });
 
 module.exports = router;
