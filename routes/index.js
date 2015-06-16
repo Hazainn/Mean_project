5c9c79859148c75099dcae50783f6fe8639efcc8
William LA PORTE <laport_w@etna-alternance.net>
2015-06-16 17:10:01 +0200

    routes de test
    une route test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router  = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Blog Jahwess' });
+});
+
+router.get('/test', function(req, res, next) {
+  res.render('test', { title: 'Blog Jahwess' });
 });
 
 module.exports = router;
