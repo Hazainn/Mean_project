cb3d06da03735eb3b479463ab75c6faa9a592f72
William LA PORTE <laport_w@etna-alternance.net>
2015-06-27 14:53:56 +0200

    modifs jean
    modifs de jean merged

---
+++ routes/comments.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app     = express();
+var title   = 'Blog Jahwes';
+
+app.route('/comment')
+	.get(function(req, res) {
+		var db         = req.db;
+		var collection = db.get('articles');
+
+		collection.find({}, {"comment" : 1}, function(e,docs) {
+			res.json(docs);
+		});
+	});
+
+app.route('/comment/:id')
+	.delete(function(req, res) {
+		var db         = req.db;
+		var collection = db.get("articles");
+
+		collection.remove({"_id" : req.params.id}, {$unset : req.body}), function(err) {
+			res.send((null === err) ? { msg: "" } : { msg: err });
+		});
+	})
+	.put(function(req, res) {
+		var db         = req.db;
+		var collection = db.get("articles");
+		console.log(req.body);
+		collection.update({"_id" : req.params.id}, {$set : req.body}, function(err, result) {
+			res.send((null === err) ? { msg: "" } : { msg: err });
+		});
+	});
+
+app.route('/article/:id_art/comment/:id_com')
+	.get(function(req, res) {
+		var db         = req.db;
+		var collection = db.get('articles');
+		console.log(req.params.id)
+		collection.find({"_id" : req.params.id_art, "comment.id" : req.params.id_com}, {"comment" : 1}, function(e,docs) {
+			res.json(docs);
+		});
+	});
+
+module.exports = app;
